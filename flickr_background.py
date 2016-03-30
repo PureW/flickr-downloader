ac9d1937cb4df44ac98bf4c3e1d853be713e3b50
Anders Bennehag <anders@bennehag.com>
2016-03-29 20:33:58 -0400

    Some more options

--- flickr_background.py
+++ flickr_background.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import json
 import gevent
+import random
 import subprocess
 import requests
 import requests_cache; requests_cache.install_cache()
 
-MIN_SIZE = (1600, 1200)
-RATIO = (1.3, 1.4)
-
 ENDPOINT = 'https://api.flickr.com/services/rest/'
+
+
+def post_step(fname, step_name):
+    """ Call some external program on finished filename """
+    STEPS = {
+        'feh-bg': ['feh', '--bg-scale', fname],
+    }
+    subprocess.check_call(STEPS[step_name])
+
 
 def base_args(apikey):
     return {
@@ -19,7 +27,8 @@ def base_args(apikey):
         'nojsoncallback': 1,
     }
 
-def get_interesting(apikey, workdir):
+
+def get_interesting(apikey, opts):
     args = {
         'method': 'flickr.interestingness.getList',
     }
@@ -29,14 +38,16 @@ def get_interesting(apikey, workdir):
 
     pics = dat['photos']['photo']
 
+    if opts['rand']:
+        random.shuffle(pics)
     for pic in pics:
         pic_id = pic['id']
-        fname = get_pic(apikey, pic_id, workdir)
+        fname = get_pic(apikey, pic_id, opts=opts)
         if fname:
             return fname
 
 
-def get_pic(apikey, pic_id, pth, min_size=MIN_SIZE):
+def get_pic(apikey, pic_id, opts):
     args = {
         'method': 'flickr.photos.getSizes',
         'photo_id': pic_id,
@@ -48,12 +59,12 @@ def get_pic(apikey, pic_id, pth, min_size=MIN_SIZE):
         print('Not allowed to download {}'.format(pic_id))
         return
     for s in dat['sizes']['size']:
-        if (int(s['width']) > min_size[0]
-                and int(s['height']) > min_size[1]):
+        if (int(s['width']) > opts['min_size'][0]
+                and int(s['height']) > opts['min_size'][1]):
             ratio = int(s['width']) / float(s['height'])
-            if ratio > RATIO[0] and ratio < RATIO[1]:
+            if ratio > opts['ratios'][0] and ratio < opts['ratios'][1]:
                 src = s['source']
-                fname = os.path.join(pth, os.path.basename(src))
+                fname = os.path.join(opts['workdir'], os.path.basename(src))
                 r = requests.get(src)
                 with open(fname, 'wb') as f:
                     f.write(r.content)
@@ -64,30 +75,53 @@ def get_pic(apikey, pic_id, pth, min_size=MIN_SIZE):
         else:
             print('Wrong size of {}'.format(pic_id))
 
-def set_bg_feh(fname):
-    subprocess.check_call(['feh', '--bg-scale', fname])
-
 
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(
-        description='Download interesting-pictures from flickr')
-    parser.add_argument('APIKEY',
+        description=("Download interesting-pictures from flickr. "
+                     "Reads flickr-apikey from env-var APIKEY_FLICKR "
+                     "or optional --apikey-flag."""))
+    parser.add_argument('--apikey',
                         help='API-key for flickr')
     parser.add_argument('--path',
                         default='/tmp/flickr-downloader',
                         help='Path to download photos to')
-    parser.add_argument('--feh',
+    parser.add_argument('-r', '--random',
+                        default=False,
                         action='store_true',
-                        help='Set desktop-background using feh')
+                        help='Randomly pick interesting photo')
+    parser.add_argument('--min-size',
+                        default='1600x1200',
+                        help='Minimum dimensions of sought photos. '
+                             'Example: "1600x1200".')
+    parser.add_argument('--ratios',
+                        default='1.3-1.4',
+                        help='Look for picture-ratios in this range. '
+                             'Example: "1.3-1.4".')
+    parser.add_argument('--post-step',
+                        choices=('feh-bg',),
+                        help='feh-bg : Set desktop-background using feh.')
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = parse_args()
 
+    apikey = args.apikey or os.getenv('APIKEY_FLICKR')
+    if not apikey:
+        print('ERROR: No apikey found')
+        sys.exit(1)
     os.makedirs(args.path, exist_ok=True)
-    fname = get_interesting(apikey= args.APIKEY, workdir=args.path)
-    if args.feh:
-        set_bg_feh(fname)
+    opts = {
+        'workdir': args.path,
+        'rand': args.random,
+        'min_size': tuple(map(int, args.min_size.split('x'))),
+        'ratios': tuple(map(float, args.ratios.split('-'))),
+    }
+    fname = get_interesting(apikey=apikey,
+                            opts=opts)
+    if args.post_step:
+        post_step(fname, args.post_step)
 
 
